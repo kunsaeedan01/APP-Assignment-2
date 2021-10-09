6dc656256a3b993aa8ecc6be7b873225b5633507
kunsaeedan01 <77623559+kunsaeedan01@users.noreply.github.com>
2021-10-09 22:15:11 +0600

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,4 +1,5 @@
 from coinScrapper import Scrapper
 
 test = Scrapper()
-test.scrap_articles("bitcoin")
+user_input = "Please, input the name of cryptocurrency to see all actual related information: "
+test.scrap_articles(user_input)
